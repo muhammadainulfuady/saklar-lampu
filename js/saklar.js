8d1c361742197e6e11724d9ff58aa73556e5a1cf
Muhammad ainul fuady <ainulfuadi1234@gmail.com>
2024-12-11 00:30:28 +0700

    saya capek tapi saya bisa hahahaha

---
+++ js/saklar.js
@@ -0,0 +1,76 @@
+function saklar() {
+  let mylamp1 = document.getElementById("mylamp1");
+  let lampu1 = document.getElementById("lampu1");
+  let lampu2 = document.getElementById("lampu2");
+  let lampu3 = document.getElementById("lampu3");
+  let lampu4 = document.getElementById("lampu4");
+  let lampu5 = document.getElementById("lampu5");
+  let lampu6 = document.getElementById("lampu6");
+  let lampu7 = document.getElementById("lampu7");
+  let lampu8 = document.getElementById("lampu8");
+  let lampu9 = document.getElementById("lampu9");
+  let lampu10 = document.getElementById("lampu10");
+
+  let mylamp2 = document.getElementById("mylamp2");
+  let mylamp3 = document.getElementById("mylamp3");
+  let mylamp4 = document.getElementById("mylamp4");
+  let mylamp5 = document.getElementById("mylamp5");
+  let mylamp6 = document.getElementById("mylamp6");
+  let mylamp7 = document.getElementById("mylamp7");
+  let mylamp8 = document.getElementById("mylamp8");
+  let mylamp9 = document.getElementById("mylamp9");
+  let mylamp10 = document.getElementById("mylamp10");
+
+  if (mylamp1.checked) {
+    lampu1.src = "assets/on.png";
+  } else {
+    lampu1.src = "assets/off.png";
+  }
+
+  if (mylamp2.checked) {
+    lampu2.src = "assets/on.png";
+  } else {
+    lampu2.src = "assets/off.png";
+  }
+
+  if (mylamp3.checked) {
+    lampu3.src = "assets/on.png";
+  } else {
+    lampu3.src = "assets/off.png";
+  }
+  if (mylamp4.checked) {
+    lampu4.src = "assets/on.png";
+  } else {
+    lampu4.src = "assets/off.png";
+  }
+  if (mylamp5.checked) {
+    lampu5.src = "assets/on.png";
+  } else {
+    lampu5.src = "assets/off.png";
+  }
+  if (mylamp6.checked) {
+    lampu6.src = "assets/on.png";
+  } else {
+    lampu6.src = "assets/off.png";
+  }
+  if (mylamp7.checked) {
+    lampu7.src = "assets/on.png";
+  } else {
+    lampu7.src = "assets/off.png";
+  }
+  if (mylamp8.checked) {
+    lampu8.src = "assets/on.png";
+  } else {
+    lampu8.src = "assets/off.png";
+  }
+  if (mylamp9.checked) {
+    lampu9.src = "assets/on.png";
+  } else {
+    lampu9.src = "assets/off.png";
+  }
+  if (mylamp10.checked) {
+    lampu10.src = "assets/on.png";
+  } else {
+    lampu10.src = "assets/off.png";
+  }
+}
